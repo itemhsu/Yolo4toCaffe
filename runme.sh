ae9241bb6a70e2f8e9013966fe969759b94fcdda
itemhsu <itemhsu@gmail.com>
2020-06-27 17:04:46 +0800

    add cat command

--- runme.sh
+++ runme.sh
@@ -1,3 +1,4 @@
 export PYTHONPATH=/home/itemhsu/src/py/MobileNet-YOLO/python/
+cat yolov4-custom_leaky.weights_0* > yolov4-custom_leaky.weights
 python darknet2caffe.py yolov4-custom_leaky.cfg yolov4-custom_leaky.weights model/yolov4-custom_leaky.prototxt model/yolov4-custom_leaky.caffemodel
 python matthew_y4_detect_one.py
